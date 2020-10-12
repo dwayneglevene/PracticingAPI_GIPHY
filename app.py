262139c3e80d75e4f73121bc542a0c56dab6b349
dwayneglevene <dwayneglevene@yahoo.com>
2020-10-12 22:50:17 +0000

    beta version

--- app.py
+++ app.py
@@ -2,15 +2,23 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-# from model import getImageUrlFrom
+from model import getImageUrlFrom
 import os
 
 # -- Initialization section --
 app = Flask(__name__)
-
-
+app.config['Giphy_Key'] = os.getenv('Giphy_Key')
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+
+@app.route('/yourgif',methods=['GET','POST'])
+def yourgif():
+    query = request.form["gifchoice"]
+    key = app.config['Giphy_Key']
+
+    # print(query)
+    image = getImageUrlFrom(query,key)
+    return render_template("yourgif.html",time = datetime.now(),image =image)
--- model.py
+++ model.py
@@ -1,2 +1,7 @@
-def getImageUrlFrom(query):
-    pass
+import requests
+
+def getImageUrlFrom(query,key):
+    endpoint = f"https://api.giphy.com/v1/gifs/search?api_key={key}&q={query}&limit=25&offset=0&rating=g&lang=en"
+    response = requests.get(endpoint).json()
+    #print(response
+    return response["data"][0]["images"]["fixed_height"]["url"]
